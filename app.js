6c270468ab8e004fc6ed7c74def549e386f4c19e
Konstantin Kiselov <constantine.kiss@gmail.com>
2022-02-12 12:57:39 +0200

    add primary files

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use((req, res) => {
+  res.status(404).json({ message: "Not found" });
+});
+
+app.use((err, req, res, next) => {
+  const { status = 500, message = "Server error" } = err;
+  res.status(status).json({ message });
+});
+
+module.exports = app;
