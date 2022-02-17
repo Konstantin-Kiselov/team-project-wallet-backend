97d5eca644601c347851ec82aae4e589bed3fdc7
Kirill Romanenko <Kyle-Roman@users.noreply.github.com>
2022-02-18 00:31:09 +0200

    working on stats controller

--- controllers/transactions/getStats.js
+++ controllers/transactions/getStats.js
@@ -1,17 +1,46 @@
 const { getUserTransactions } = require("../../services/transactions/transactionsService");
 
-const getStats = async (req, res, next) => {
-    try {
-      const {page = 1, limit = 10, categorie} = req.query
-      const skip = (page - 1) * limit
-      const {_id} = req.user
-      const object = `ObjectId("${_id}")`
-      const stats = await getUserTransactions({owner: object.replace(/\\/, '')}, '', {skip, limit: +limit, categorie})
-      
-      res.json(stats)
-    } catch (error) {
-      next(error)
-    }
-  };
+const getStatsController = async (req, res, next) => {
+  try {
+    const { _id } = req.user;
 
-  module.exports = getStats;
+    const transactions = await getUserTransactions(_id);
+    res.json(transactions);
+  // const getStats = async ( userId, query) => {
+  //   try {
+  //     if (!query?.year || !query?.income) {
+  //       return null;
+  //     }
+  
+  //     let statsArr = [];
+  //     const transactionsByOwner = await Transaction.find({ owner: userId });
+  //     let total = 0;
+  
+  //     const result = transactionsByOwner.reduce(
+  //       (acc, transaction) => {
+  //         const { income, amount, date } = transaction;
+
+  //     const getMonthByDate = (date) => {
+  //       const newDate = new Date(date);
+  //       return newDate.getMonth();
+  //     };
+  
+  //     const month = getMonthByDate(date);
+
+  //         if (income === query.income && month >= startDate && month <= endDate) {
+  //           statsArr[month].total += amount;
+  //           total += amount;
+  //         }
+  
+  //         return [...acc];
+  //       },
+  //       [...summary]
+  //     );
+  //     return { result, total };
+    } catch (error) {
+      next(error);
+    }
+  // };
+};
+
+  module.exports = getStatsController;
