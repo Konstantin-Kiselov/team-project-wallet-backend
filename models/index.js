c548dfb30a907edebc4490e358ede92093c174b5
constantine <79282281+Konstantin-Kiselov@users.noreply.github.com>
2022-02-20 20:02:09 +0200

    Revert "Added google auth and finished swagger alfa version"

--- models/index.js
+++
@@ -1,9 +0,0 @@
-const { User } = require("./user");
-const { Transaction } = require("./transaction");
-const { TransactionCategory } = require("./transactionCategory");
-
-module.exports = {
-  User,
-  Transaction,
-  TransactionCategory,
-};
