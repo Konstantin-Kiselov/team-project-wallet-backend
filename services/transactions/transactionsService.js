f26cb05fc9cd1f4c66eae407dc3402a415685148
Yaroslav Kulyk <kulyk.yaroslav@gmail.com>
2022-03-20 15:38:17 +0200

    refactored to mongodb native methods

--- services/transactions/transactionsService.js
+++ services/transactions/transactionsService.js
@@ -21,69 +21,105 @@ const addTransaction = async (userId, balance, transaction) => {
 };
 
 const getUserTransactions = async (userId) => {
-  const transactions = await Transaction.find({
-    owner: userId,
-  })
-    .populate({ path: "category", model: TransactionCategory })
-    .sort({ createdAt: "desc" });
+  const transactions = await Transaction.aggregate([
+    {
+      $match: {
+        owner: userId,
+      },
+    },
+    {
+      $lookup: {
+        from: "categories",
+        localField: "category",
+        foreignField: "_id",
+        as: "category",
+      },
+    },
+    { $unwind: "$category" },
+    {
+      $set: {
+        date: { $dateToString: { date: "$createdAt", format: "%d.%m.%Y" } },
+      },
+    },
+    { $unset: ["createdAt", "updatedAt"] },
+    { $sort: { createdAt: -1 } },
+  ]);
 
-  const options = { day: "numeric", month: "numeric", year: "2-digit" };
-  const response = transactions.map(
-    ({ _id, income, category, comment, amount, total, owner, createdAt }) => {
-      return {
-        _id,
-        income,
-        category,
-        comment,
-        amount,
-        total,
-        owner,
-        date: createdAt.toLocaleDateString("ru-RU", options),
-      };
-    }
-  );
-
-  return response;
+  return transactions;
 };
 
 const getTransactionsStatistics = async (userId, year, month) => {
-  const transactions = await Transaction.find(
+  const result = await Transaction.aggregate([
     {
-      owner: userId,
-      createdAt: {
-        $gte: new Date(year, month),
-        $lt: new Date(year, month + 1),
+      $match: {
+        owner: userId,
+        createdAt: {
+          $gte: new Date(year, month),
+          $lt: new Date(year, Number(month) + 1),
+        },
       },
     },
-    "-updatedAt"
-  )
-    .populate({ path: "category", model: TransactionCategory })
-    .sort({ createdAt: "desc" });
+    {
+      $facet: {
+        stats: [
+          { $match: { income: false } },
+          {
+            $lookup: {
+              from: "categories",
+              localField: "category",
+              foreignField: "_id",
+              as: "category",
+            },
+          },
+          {
+            $group: {
+              _id: "$category",
+              amount: { $sum: "$amount" },
+            },
+          },
+          {
+            $sort: { amount: -1 },
+          },
+          {
+            $project: {
+              _id: 0,
+              name: { $first: "$_id.name" },
+              amount: "$amount",
+            },
+          },
+        ],
+        income: [
+          { $match: { income: true } },
+          {
+            $group: {
+              _id: null,
+              total: { $sum: "$amount" },
+            },
+          },
+          { $unset: "_id" },
+        ],
+        expense: [
+          { $match: { income: false } },
+          {
+            $group: {
+              _id: null,
+              total: { $sum: "$amount" },
+            },
+          },
+          { $unset: "_id" },
+        ],
+      },
+    },
+    {
+      $project: {
+        stats: "$stats",
+        expense: { $first: "$expense.total" },
+        income: { $first: "$income.total" },
+      },
+    },
+  ]);
 
-  const getExpenseCategories = (transactions) => {
-    return transactions
-      .filter((transaction) => !transaction.income)
-      .flatMap((transaction) => transaction.category?.name)
-      .filter((category, index, array) => array.indexOf(category) === index);
-  };
-
-  const stats = getExpenseCategories(transactions).map((cat) => {
-    const totals = transactions
-      .filter((transaction) => transaction.category?.name === cat)
-      .reduce((acc, item) => item.amount + acc, 0);
-
-    return { name: cat, amount: totals };
-  });
-
-  const totalIncome = transactions
-    .filter((transaction) => transaction.income)
-    .reduce((acc, item) => item.amount + acc, 0);
-
-  const totalExpense = transactions
-    .filter((transaction) => !transaction.income)
-    .reduce((acc, item) => item.amount + acc, 0);
-
-  return { stats, income: totalIncome, expense: totalExpense };
+  return result[0];
 };
 
 const addTransactionCategory = async (userId, category) => {
